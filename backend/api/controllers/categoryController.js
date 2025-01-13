dc41a5cc8f248de48143d7a09d7ee573289b0ddf
LivCenteno <livandrec@gmail.com>
2025-01-13 13:54:40 +0800

    Created ViewAllCategories by status and Update Category Status

--- backend/api/controllers/categoryController.js
+++ backend/api/controllers/categoryController.js
@@ -6,7 +6,7 @@ const insertCategory = async (req, res, next) => {
     let name = req.body.name;
 
     if (!name) {
-      return res.status(400).json({
+      return res.status(404).json({
         successful: false,
         message: "Name is missing",
       });
@@ -36,4 +36,66 @@ const insertCategory = async (req, res, next) => {
     });
   }
 };
-module.exports = { insertCategory };
+
+const viewAllCategories = async (req, res, next) => {
+  const status = req.body.status;
+  try {
+    const data = await knex("category").where("category.status", status);
+
+    if (data.length == 0) {
+      return res.status(400).json({
+        successful: false,
+        message: `No Category with Status:${status} Found`,
+      });
+    } else {
+      return res.status(200).json({
+        successful: true,
+        message: "Successfully Retrieved All Categories",
+        data: data,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      successful: false,
+      message: err,
+    });
+  }
+};
+
+const updateCategoryStatus = async (req, res, next) => {
+  let id = req.params.id;
+  let status = req.body.status;
+  if (!id || !status) {
+    return res.status(404).json({
+      successful: false,
+      message: "Id or Status not Found",
+    });
+  } else {
+    try {
+      const data = await knex("category").where("id", id).first();
+
+      if ((data.length = 0)) {
+        return res.status(400).json({
+          successful: false,
+          message: `There is no Category with the id of ${id}`,
+        });
+      } else {
+        await knex("category").where("id", id).update("status", status);
+        return res.status(200).json({
+          successful: true,
+          message: "Successfully Updated Category Status.",
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({
+        successful: false,
+        message: err,
+      });
+    }
+  }
+};
+module.exports = {
+  insertCategory,
+  viewAllCategories,
+  updateCategoryStatus,
+};
--- backend/api/routers/categoryRouter.js
+++ backend/api/routers/categoryRouter.js
@@ -2,5 +2,8 @@ const express = require("express");
 const categoryRouter = express();
 const categoryController = require("../controllers/categoryController");
 
-categoryRouter.post("/create", categoryController.insertCategory);
+categoryRouter.post("/create", categoryController.insertCategory); //Create Another Category
+categoryRouter.get("/view/all", categoryController.viewAllCategories); //Get All Categories
+categoryRouter.put("/update/:id", categoryController.updateCategoryStatus); //Update Category Status
+
 module.exports = categoryRouter;
